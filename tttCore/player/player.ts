79773f3bf61378bb87623c5c7f221cd24f5b0c9c
Danny <perez6736@gmail.com>
2026-02-18 22:42:38 +0900

    player object for tictactoe

---
+++ tttCore/player/player.ts
@@ -0,0 +1,13 @@
+type playerMark = 'X' | 'O';
+
+export class Player {
+  private player: playerMark;
+
+  constructor(player: playerMark) {
+    this.player = player;
+  }
+
+  getPlayer(): playerMark {
+    return this.player;
+  }
+}
