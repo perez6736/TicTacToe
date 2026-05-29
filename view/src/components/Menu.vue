8b9635a8c263e92c111e31a70edef57ea0ad2604
Danny <perez6736@gmail.com>
2026-05-29 23:57:05 +0900

    vue components ..kewl

---
+++ view/src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div class="menu-container">
+      <div class="menu-title">Tic Tac Toe</div>
+      <div class="menu-options">
+        <div class="options-container">
+          <div class="options-option">1 Player</div>
+          <div class="options-option">2 Player</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style></style>
